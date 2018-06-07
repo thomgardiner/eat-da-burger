40db5162cffc2a74a6c569d7901af1bfc3371aa9
Thomas Gardiner <thomgardiner@gmail.com>
2018-06-07 12:39:22 -0600

    initial commit + basic routing

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconburger', FALSE);
+
+SELECT * FROM burgers;
